1d89144d4c4a6344414c037fe1d8d3fd6d146db6
Aisha Kothare <aishakothare31@gmail.com>
2022-10-21 14:11:39 -0400

    added date parameter to the messages model

--- models/Messages.ts
+++ models/Messages.ts
@@ -13,4 +13,5 @@ export default class Messages {
     private sender:User | null = null
     private receiver:User | null = null
     private message:String | null = null
+    private sentOn: Date | null = null
 }
--- mongoose/MessagesScehma.ts
+++ mongoose/MessagesScehma.ts
@@ -9,6 +9,7 @@ import mongoose, { Schema } from "mongoose";
 const MessagesSchema = new mongoose.Schema({
    sender: {type: Schema.Types.ObjectId, ref:"UserModel"},
    receiver: {type: Schema.Types.ObjectId, ref:"UserModel"},
-   message: {type: String}
+   message: {type: String},
+   sentOn:{type: Date, default: Date.now}
 }, {collection: 'message'});
 export default MessagesSchema;
